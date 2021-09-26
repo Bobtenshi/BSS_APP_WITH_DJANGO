b893bc3ea9c1616507bf765c867b0804d832865b
bobtenshi <hei8maji@gmail.com>
2021-09-27 00:31:00 +0900

    fix?

---
+++ bss/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(req):
+    return HttpResponse("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
